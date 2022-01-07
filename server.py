f4eedc5adac0d40f7d5ce7fffe98ab9749ccfba8
lyy <1411636691@qq.com>
2022-01-08 05:48:56 +0800

    Changes

--- server.py
+++ server.py
@@ -66,4 +66,4 @@ def disconnect_msg():
 
 if __name__ == '__main__':
     print("conent http://127.0.0.1:5000")
-    socketio.run(app, host='127.0.0.1', port=5000)
+    socketio.run(app, host='1.117.37.235', port=5000)
